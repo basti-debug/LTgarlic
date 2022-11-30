3761c17a0acc6e91b95ac64930d7a8e5f5e5cae1
basti-debug <basti.debug@gmail.com>
2022-11-30 17:52:24 +0100

    resolving merging error

--- LTgarlic/LTgarlic/MainWindow.xaml.cs
+++ LTgarlic/LTgarlic/MainWindow.xaml.cs
@@ -6,18 +6,10 @@ public sealed partial class MainWindow : WindowEx
 {
     public MainWindow()
     {
-<<<<<<< HEAD
-        public MainWindow()
-        {
-            InitializeComponent();
-
-        }
-=======
         InitializeComponent();
 
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         Content = null;
         Title = "AppDisplayName".GetLocalized();
->>>>>>> uibasic
     }
 }
