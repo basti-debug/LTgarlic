89cd28897e4cdfc0baaa2ac2a6563c62f72230c9
basti-debug <basti.debug@gmail.com>
2023-01-23 20:00:49 +0100

    looks like pagecreator went missing, here is he back

---
+++ LTgarlic/LTgarlic/PageCreator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LTgarlic;
+public class neweditingpage : Page
+{
+    public neweditingpage()
+    {
+        var drawcanvas = new Canvas();
+        var hotbar = new CommandBar();
+
+        var wirebutton = new AppBarButton
+        {
+            Icon = new SymbolIcon(Symbol.Edit),
+            Label = "wire mode"
+        };
+
+        var simbutton = new AppBarButton
+        {
+            Icon = new SymbolIcon(Symbol.Calculator),
+            Label = "simulate"
+        };
+
+        hotbar.PrimaryCommands.Add(wirebutton);
+        hotbar.SecondaryCommands.Add(simbutton);
+
+        this.Content = drawcanvas;
+    }
+}
