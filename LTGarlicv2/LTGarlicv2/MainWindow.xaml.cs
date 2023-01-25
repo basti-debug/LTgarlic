654491d25be82165e99b4adb59b32c41cf813d7c
basti <84239503+basti-debug@users.noreply.github.com>
2023-01-25 23:11:28 +0100

    new version with fixed ui navigation

---
+++ LTGarlicv2/LTGarlicv2/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Windows;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LTGarlicv2
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            MainLTWindow.ExtendsContentIntoTitleBar = true;
+            MainLTWindow.SetTitleBar(null);
+
+            Canvas mainCanva = new Canvas();
+            mainCanva.Height = 1000;
+            mainCanva.Width = 1000;
+
+            TextBlock Title = new TextBlock();
+            Title.Text = "Welcome to LTGarlic";
+            Title.FontSize = 30;
+
+            Button createbutton = new Button();
+            createbutton.Content = "Create a new File";
+            createbutton.Height = 30;
+            createbutton.Click += creatButtononClick;
+
+            Button openbutton = new Button();
+            openbutton.Content = "Open a File";
+
+            Button openlib = new Button();
+            openlib.Content = "Open a Libary";
+
+
+            // Moving Objects inside the Canvas
+            Canvas.SetLeft(Title, -200);
+            Canvas.SetTop(Title,100);
+
+            Canvas.SetLeft(createbutton, -200);
+            Canvas.SetTop(createbutton, 200);
+
+            mainCanva.Children.Add(createbutton);
+            mainCanva.Children.Add(openbutton);
+            mainCanva.Children.Add(openlib);
+            mainCanva.Children.Add(Title);
+
+            
+            contentFrame.Content = mainCanva;
+
+            nvSample.SelectionChanged += NvSample_SelectionChanged();
+
+
+        }
+
+        async void NvSample_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            string selecteditem = nvSample.SelectedItem.ToString();
+            PageBuilder newpage = new PageBuilder();
+            newpage.displayFilePage(contentFrame, TitleBlock, selecteditem);
+        }
+
+        async void  creatButtononClick(object sender, RoutedEventArgs args)
+        {
+            TeachingTip errortip = new TeachingTip();
+
+            //var hwnd = 
+
+            //var picker = new FileSavePicker();
+            //WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
+            //picker.FileTypeChoices.Add("Spice Circuit", new List<string>() { ".asc" });
+            //picker.SuggestedFileName = "cicuit";
+            //var file = await picker.PickSaveFileAsync();
+
+            //var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            //savePicker.SuggestedStartLocation =
+            //    Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            //savePicker.FileTypeChoices.Add("Spice Circuit", new List<string>() { ".asc" });
+            //savePicker.SuggestedFileName = "circuit1";
+
+            //Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            //if (file != null)
+            //{
+            //     // Prevent updates to the remote version of the file until
+            //    // we finish making changes and call CompleteUpdatesAsync.
+            //    Windows.Storage.CachedFileManager.DeferUpdates(file);
+            //    // write to file
+            //    await Windows.Storage.FileIO.WriteTextAsync(file, file.Name);
+            //    // Let Windows know that we're finished changing the file so
+            //    // the other app can update the remote version of the file.
+            //    // Completing updates may require Windows to ask for user input.
+            //    Windows.Storage.Provider.FileUpdateStatus status =
+            //    await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+            //    if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+            //    {
+            //        errortip.Title = "File Saved";
+            //        errortip.Subtitle = "File " + file.Name + " was saved. You can open it on the left side";
+            //        errortip.IsOpen = true;
+            //    }
+            //    else
+            //    {
+            //        errortip.Title = "File couldn't Saved";
+            //        errortip.Subtitle = "File " + file.Name + " wasnt saved, please retry";
+            //        errortip.IsOpen = true;
+            //    }
+            //}
+            //else
+            //{
+            //    errortip.Title = "A error accoured ";
+            //    errortip.IsOpen = true;
+            //}
+
+
+
+            NavigationViewItem newproject = new NavigationViewItem();
+            newproject.Content = "file.Name";
+            nvSample.MenuItems.Add(newproject);
+
+        }
+
+    }
+}
---
+++ LTGarlicv2/LTGarlicv2/PageBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTGarlicv2
+{
+    public class PageBuilder
+    {
+        public void displayFilePage(Frame currentframe, TextBlock Title, string filename)
+        {
+            Title.Text = filename;
+            Canvas canvas = new Canvas();
+            var hotbar = new CommandBar();
+
+            var wirebutton = new AppBarButton
+            {
+                Icon = new SymbolIcon(Symbol.Edit),
+                Label = "wire mode"
+            };
+
+            var simbutton = new AppBarButton
+            {
+                Icon = new SymbolIcon(Symbol.Calculator),
+                Label = "simulate"
+            };
+
+            hotbar.PrimaryCommands.Add(wirebutton);
+            hotbar.SecondaryCommands.Add(simbutton);
+
+            canvas.Children.Add(hotbar);
+
+            currentframe.Content = canvas;
+
+        }
+    }
+}
